dcc1cb0861e2aabeaf66e9fb417fe2be533981a6
eodudrepublic <eodudrepublic@gmail.com>
2025-01-15 20:50:55 +0900

    merge - 현정이가 작업한 mypage 내용 병합

--- lib/view/mypage/mypage_view.dart
+++ lib/view/mypage/mypage_view.dart
@@ -1,10 +1,23 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import '../../common/app_colors.dart';
+import '../../common/server_url.dart';
+import '../../common/utils/logger.dart';
 import '../../common/widget/custom_bottom_navigation_bar.dart';
+import '../../view_model/mypage/mypage_controller.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class MyPageView extends StatelessWidget {
-  const MyPageView({super.key});
+  final MyPageController _controller = Get.put(MyPageController());
+
+  MyPageView({super.key}) {
+    // 프로필 데이터 로드
+    _controller.loadUserProfile();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,34 +29,45 @@ class MyPageView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // 프로필 섹션
               Row(
                 children: [
-                  CircleAvatar(
-                    radius: 40.r,
-                    backgroundColor: AppColors.borderGreyColor,
-                    backgroundImage: null, // 프로필 이미지는 null로 기본 설정
-                    child: Icon(Icons.person, size: 40.r, color: Colors.white),
-                  ),
+                  // 프로필 사진
+                  Obx(() => CircleAvatar(
+                        radius: 40.r,
+                        backgroundColor: AppColors.borderGreyColor,
+                        backgroundImage: _controller.profileImage.value != null
+                            ? NetworkImage(_controller.profileImage.value!)
+                            : null,
+                        child: _controller.profileImage.value == null
+                            ? Icon(Icons.person,
+                                size: 40.r, color: Colors.white)
+                            : null,
+                      )),
                   SizedBox(width: 10.w),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "이현정", // 닉네임
-                        style: TextStyle(
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                      // 닉네임
+                      Obx(() => Text(
+                            _controller.name.value.isNotEmpty
+                                ? _controller.name.value
+                                : "닉네임을 불러오는 중...",
+                            style: TextStyle(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          )),
+                      // 운동 완료 일수
+                      Obx(
+                        () => Text(
+                          "총 ${_controller.exerciseDays.value}일 운동 완료",
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "총 10일 운동 완료", // 운동 완료 정보
-                        style: TextStyle(
-                          fontSize: 14.sp,
-                          color: Colors.grey,
-                        ),
-                      ),
+                      )
                     ],
                   ),
                 ],
@@ -53,14 +77,28 @@ class MyPageView extends StatelessWidget {
               Section(
                 title: "친구 관리",
                 items: [
-                  SectionItem(title: "친구 추가", onTap: () => print("친구 추가 클릭")),
+                  SectionItem(
+                    title: "친구 추가",
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => QRCodeScannerScreen(
+                          onScanned: (scannedUserId) {
+                            _controller.addFriendFromQrCode(scannedUserId);
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
                   SectionItem(
                     title: "내 QR 코드",
-                    onTap: () => _showQrCodeModal(context), // QR 코드 모달 호출
+                    onTap: () => showAddFriendDialog(
+                        context, _controller.userId.toString()), // QR 코드 모달 호출
                   ),
                   SectionItem(
                     title: "친구 목록",
-                    onTap: () => _showFriendListModal(context), // 친구 목록 모달 호출
+                    onTap: () => _showFriendListModal(
+                        context, _controller.userId.toString()), // 친구 목록 모달 호출
                   ),
                 ],
               ),
@@ -72,11 +110,12 @@ class MyPageView extends StatelessWidget {
                   SectionItem(
                       title: "테마",
                       onTap: () {
-                        // TODO : Get.toNamed('/set_theme');
-                        print("테마 클릭");
+                        Log.info("테마 클릭");
+                        Get.toNamed('/theme');
                       }),
-                  SectionItem(title: "앱 잠금 등록", onTap: () => print("앱 잠금 클릭")),
-                  SectionItem(title: "보이스", onTap: () => print("보이스 클릭")),
+                  SectionItem(
+                      title: "앱 잠금 등록", onTap: () => Log.info("앱 잠금 클릭")),
+                  SectionItem(title: "보이스", onTap: () => Log.info("보이스 클릭")),
                 ],
               ),
               SizedBox(height: 20.h),
@@ -84,17 +123,29 @@ class MyPageView extends StatelessWidget {
               Section(
                 title: "이용 규칙",
                 items: [
-                  SectionItem(title: "이용 약관", onTap: () => print("이용 약관 클릭")),
                   SectionItem(
-                      title: "개인정보 처리 방침", onTap: () => print("개인정보 처리 방침 클릭")),
+                    title: "이용 약관",
+                    onTap: () {
+                      _showTermsModal(context, "이용 약관",
+                          "김대영은 신이야. 자세한 내용은 앱 관리자를 통해 확인하세요.");
+                    },
+                  ),
+                  SectionItem(
+                    title: "개인정보 처리 방침",
+                    onTap: () {
+                      _showTermsModal(context, "개인정보 처리 방침",
+                          "아 힘들엉. 사용자의 데이터는 안전하게 보호됩니다.");
+                    },
+                  ),
                 ],
               ),
+
               SizedBox(height: 20.h),
               // 로그아웃 버튼
               Align(
                 alignment: Alignment.centerRight,
                 child: TextButton(
-                  onPressed: () => print("로그아웃 클릭"),
+                  onPressed: () => Log.info("로그아웃 클릭"),
                   child: Text(
                     "로그아웃",
                     style: TextStyle(fontSize: 16.sp, color: Colors.grey),
@@ -114,60 +165,65 @@ class MyPageView extends StatelessWidget {
     );
   }
 
-  // QR 코드 모달
-  void _showQrCodeModal(BuildContext context) {
-    showModalBottomSheet(
+  // 내 QR 코드 다이얼로그
+  void showAddFriendDialog(BuildContext context, String userId) {
+    showDialog<void>(
       context: context,
-      backgroundColor: Colors.transparent,
-      builder: (context) {
-        return Container(
-          height: 300.h,
-          margin: EdgeInsets.all(20.r),
-          padding: EdgeInsets.all(20.r),
-          decoration: BoxDecoration(
-            color: AppColors.textBackgroundColor,
-            borderRadius: BorderRadius.circular(15.r),
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // backgroundColor: Colors.white,
+          backgroundColor: Color(0xFF1A1A1A),
+          title: const Text(
+            '내 QR 코드',
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "내 QR 코드",
-                style: TextStyle(
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          content: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // 자식 위젯 크기만큼만 크기 설정
+              children: [
+                const Text(
+                  '아래 QR 코드를 스캔하여 친구를 추가하세요.',
+                  style: TextStyle(fontSize: 14, color: Color(0xFF777777)),
                 ),
-              ),
-              SizedBox(height: 20.h),
-              Container(
-                width: 150.w,
-                height: 150.w,
-                color: Colors.white, // 임시 QR 코드 배경색
-                child: Center(
-                  child: Text(
-                    "QR CODE",
-                    style: TextStyle(
-                      fontSize: 16.sp,
-                      color: Colors.black,
-                    ),
-                  ),
+                const SizedBox(height: 16.0),
+                // QrImageView(
+                //   data: jsonEncode({"user_id": userId}), // QR 코드 데이터
+                //   version: QrVersions.auto, // QR 코드 버전 자동
+                //   size: 200.0, // QR 코드 크기
+                // ),
+                QrImageView(
+                  data: jsonEncode({"user_id": userId}), // QR 코드 데이터
+                  version: QrVersions.auto, // QR 코드 버전 자동
+                  size: 200.0, // QR 코드 크기
+                  foregroundColor: Colors.white, // QR 코드 색상 (흰색)
                 ),
-              ),
-              SizedBox(height: 20.h),
-              Text(
-                "QR 코드를 스캔하여 친구를 추가하세요",
-                style: TextStyle(fontSize: 14.sp, color: Colors.grey),
-              ),
-            ],
+              ],
+            ),
           ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                '닫기',
+                style: TextStyle(color: Color(0xFFEE1171)),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
     );
   }
+}
 
-  // 친구 목록 모달
-  void _showFriendListModal(BuildContext context) {
+void _showFriendListModal(BuildContext context, String userId) async {
+  try {
+    final friends = await fetchFriends(userId);
+
     showModalBottomSheet(
       context: context,
       backgroundColor: Colors.transparent,
@@ -193,23 +249,125 @@ class MyPageView extends StatelessWidget {
               ),
               SizedBox(height: 20.h),
               Expanded(
-                child: ListView.builder(
-                  itemCount: 10, // 예제 데이터: 10명의 친구
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(
-                        "친구 $index",
-                        style: TextStyle(fontSize: 14.sp, color: Colors.white),
+                child: friends.isEmpty
+                    ? Center(
+                        child: Text(
+                          "친구가 없습니다.",
+                          style:
+                              TextStyle(fontSize: 14.sp, color: Colors.white),
+                        ),
+                      )
+                    : ListView.builder(
+                        itemCount: friends.length,
+                        itemBuilder: (context, index) {
+                          final friend = friends[index];
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: friend['profile_image'] != null
+                                  ? NetworkImage(friend['profile_image'])
+                                  : null,
+                              child: friend['profile_image'] == null
+                                  ? const Icon(Icons.person)
+                                  : null,
+                            ),
+                            title: Text(
+                              friend['nickname'] ?? "Unknown",
+                              style: TextStyle(
+                                  fontSize: 14.sp, color: Colors.white),
+                            ),
+                          );
+                        },
                       ),
-                      onTap: () => print("친구 $index 클릭"),
-                    );
-                  },
-                ),
               ),
             ],
           ),
         );
       },
+    );
+  } catch (e) {
+    // 오류 처리
+    Log.info("Error displaying friend list: $e");
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Color(0xFF1A1A1A),
+          title: const Text(
+            '친구 목록',
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+          content: const Text(
+            "친구가 없습니다.",
+            style: TextStyle(color: Color(0xFF777777)), // 흰색 텍스트 스타일 추가
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                '닫기',
+                style: TextStyle(color: Color(0xFFEE1171)),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+Future<List<Map<String, dynamic>>> fetchFriends(String userId) async {
+  final String apiUrl = "$serverUrl:8000/users/$userId/friends";
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      return data.map((friend) => friend as Map<String, dynamic>).toList();
+    } else {
+      Log.info("Failed to load friends: ${response.statusCode}");
+      throw Exception("Failed to load friends");
+    }
+  } catch (e) {
+    Log.info("Error fetching friends: $e");
+    throw Exception("Error fetching friends");
+  }
+}
+
+class QRCodeScannerScreen extends StatelessWidget {
+  final Function(String) onScanned;
+
+  QRCodeScannerScreen({required this.onScanned});
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+    // 첨부된 이미지에서 색상을 추출한 뒤 사용
+    final Color appBarBackgroundColor =
+        Color(0xFF1A1A1A); // 임의의 색상 (검정색에 가까운 색)
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "QR 코드 스캔",
+          style: TextStyle(color: Colors.white), // 텍스트 색상을 흰색으로 설정
+        ),
+        backgroundColor: appBarBackgroundColor, // 배경색 설정
+        iconTheme: const IconThemeData(color: Colors.white), // 뒤로 가기 아이콘 색상
+      ),
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: (controller) {
+          controller.scannedDataStream.listen((scanData) {
+            onScanned(scanData.code ?? "");
+            controller.dispose();
+            Navigator.pop(context);
+          });
+        },
+      ),
     );
   }
 }
@@ -278,4 +436,61 @@ class SectionItem {
   final VoidCallback onTap;
 
   SectionItem({required this.title, required this.onTap});
+}
+
+// 이용 약관 모달창
+void _showTermsModal(BuildContext context, String title, String content) {
+  showModalBottomSheet(
+    context: context,
+    backgroundColor: Colors.transparent,
+    builder: (context) {
+      return Container(
+        padding: EdgeInsets.all(20.r),
+        decoration: BoxDecoration(
+          color: AppColors.textBackgroundColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.r),
+            topRight: Radius.circular(20.r),
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Text(
+                content,
+                style: TextStyle(
+                  fontSize: 14.sp,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    "닫기",
+                    style: TextStyle(
+                        fontSize: 16.sp, color: AppColors.primaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
--- lib/view_model/mypage/mypage_controller.dart
+++ lib/view_model/mypage/mypage_controller.dart
@@ -1,28 +1,116 @@
 import 'dart:convert';
 import 'package:get/get.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../common/server_url.dart';
+import '../../common/utils/logger.dart';
 
 class MyPageController extends GetxController {
-  // 사용자 이름
-  final RxString name = "이현정".obs;
-
-  // 사용자 프로필 이미지 URL (초기값 null)
+  final RxString name = "".obs;
   final RxnString profileImage = RxnString();
+  final RxInt exerciseDays = 0.obs;
+  final RxInt userId = 0.obs;
 
-  // 총 운동 완료 일수
-  final RxInt exerciseDays = 10.obs;
+  @override
+  void onInit() {
+    super.onInit();
+    loadUserProfile();
+  }
 
-  get showDetails => null;
+  // 사용자 프로필 및 ID 로드
+  Future<void> loadUserProfile() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final id = prefs.getInt('user_id');
+      Log.info("Loaded user_id from SharedPreferences: $id");
 
-  // 데이터 초기화 (백엔드 연동 시 업데이트 가능)
-  void initializeData({required String userName, String? userImage, required int totalDays}) {
-    name.value = userName;
-    profileImage.value = userImage;
-    exerciseDays.value = totalDays;
+      if (id != null) {
+        userId.value = id;
+        await fetchUserProfile(id);
+      } else {
+        Log.info("User ID not found in SharedPreferences");
+      }
+    } catch (e) {
+      Log.info("Error loading user profile: $e");
+    }
+  }
+
+  // 사용자 프로필 서버에서 가져오기
+  Future<void> fetchUserProfile(int id) async {
+    try {
+      Log.info("Fetching user profile for ID: $id");
+      final response = await http.get(
+        Uri.parse("$serverUrl:8000/users/$id/profile"),
+      );
+
+      Log.info("Response status: ${response.statusCode}");
+      Log.info("Response body: ${response.body}");
+
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(utf8.decode(response.bodyBytes));
+        Log.info("Decoded data: $decodedData");
+
+        name.value = decodedData["nickname"] ?? "Anonymous";
+        profileImage.value = decodedData["profile_image"];
+        exerciseDays.value = decodedData["completed_days"] ?? 0;
+
+        Log.info("Updated name: ${name.value}");
+        Log.info("Updated profileImage: ${profileImage.value}");
+        Log.info("Updated exerciseDays: ${exerciseDays.value}");
+      } else {
+        Log.info("Failed to fetch user profile: ${response.statusCode}");
+      }
+    } catch (e) {
+      Log.info("Error fetching user profile: $e");
+    }
+  }
+
+  // QR 코드 스캔 및 친구 추가
+  Future<void> addFriendFromQrCode(String scannedUserId) async {
+    try {
+      final response = await http.post(
+        Uri.parse("$serverUrl:8000/friends"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({
+          "scanned_user_id": userId.value,
+          "qr_user_id": int.parse(scannedUserId),
+        }),
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        Log.info("Friend added successfully.");
+        Get.snackbar("친구 추가 성공", "친구가 성공적으로 추가되었습니다.");
+      } else {
+        Log.info("Failed to add friend: ${response.statusCode}");
+        Get.snackbar("친구 추가 실패", "상태 코드: ${response.statusCode}");
+      }
+    } catch (e) {
+      Log.info("Error adding friend: $e");
+      Get.snackbar("친구 추가 실패", "네트워크 오류가 발생했습니다.");
+    }
+  }
+
+  // Future<List<Map<String, dynamic>>> fetchFriends(String userId) async {
+  //   final String apiUrl = "$serverUrl:8000/users/$userId/friends";
+  //   try {
+  //     final response = await http.get(Uri.parse(apiUrl));
+  //
+  //     if (response.statusCode == 200) {
+  //       final List<dynamic> data = jsonDecode(response.body);
+  //       return data.map((friend) => friend as Map<String, dynamic>).toList();
+  //     } else {
+  //       Log.info("Failed to load friends: ${response.statusCode}");
+  //       throw Exception("Failed to load friends");
+  //     }
+  //   } catch (e) {
+  //     Log.info("Error fetching friends: $e");
+  //     throw Exception("Error fetching friends");
+  //   }
+  // }
+
+  // QR 코드 데이터 생성
+  String generateQrCode() {
+    Log.info("Debug: Generating QR Code for userId: ${userId.value}");
+    return userId.value.toString(); // user_id를 QR 코드 데이터로 사용
   }
 }
-
-// qr 코드 스캐너 - 친구 추가
